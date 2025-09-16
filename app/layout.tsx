8ce2a51170903bf4fabb656cd592578abebc1a42
hugobel <hola@hugobeltran.com>
2025-09-16 06:04:51 -0700

    chore: better fluid layout for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,31 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { IBM_Plex_Sans } from "next/font/google";
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-prisma.vercel.app'),
-  title: 'Vercel Postgres Demo with Prisma',
-  description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
-}
+  metadataBase: new URL("https://postgres-prisma.vercel.app"),
+  title: "Velora Capital",
+  description: "Manage, create, and analyze loans—all in one place.",
+};
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} bg-background text-primary`}>
-        <main className="relative flex min-h-screen flex-col items-center pt-24">
-          <div className="flex flex-col items-center w-full max-w-7xl ">
-            {children}
-          </div>
+      <body className={`${ibmPlexSans.variable} bg-background text-primary`}>
+        <main className="relative flex min-h-screen flex-col items-center pt-16 sm:pt-20 lg:pt-24 px-4">
+          <div className="w-full max-w-7xl">{children}</div>
         </main>
       </body>
     </html>
-  )
+  );
 }
